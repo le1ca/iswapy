77780722eadbb17d697e8c7d2320f730ef40b96c
Travis Mick <root@lo.calho.st>
2016-02-03 00:51:51 -0700

    implement frame splitter

--- iswapy/processing/split_frames.py
+++ iswapy/processing/split_frames.py
@@ -1,10 +1,20 @@
 from base_classes import processing_cls
+import skimage.util
 
 @processing_cls.register("split_frames")
 class split_frames (processing_cls):
 
-        def __init__(self):
-                pass
+        def __init__(self, width, space, buffer=0):
+                self._w = width
+                self._s = space
+                self._b = buffer
                 
         def process(self, img):
-                return [img]
+                ret = []
+                accum = 0
+                _, w = img.shape
+                while accum + self._w < w:
+                        ret.append(skimage.util.crop(img, ((0, 0), (accum, w - (accum + self._w + self._b/2)))))
+                        print("Split frame %d pixels wide." % self._w)
+                        accum += self._w + self._s - self._b/2
+                return ret
