35caf4fa3f19b1dd2a94fd70b774a96972bf78d5
Travis Mick <root@lo.calho.st>
2016-02-03 00:51:33 -0700

    implement sigmoidal contrast filter

--- iswapy/processing/sigmoidal_contrast.py
+++ iswapy/processing/sigmoidal_contrast.py
@@ -1,10 +1,15 @@
 from base_classes import processing_cls
+import skimage.exposure
 
 @processing_cls.register("sigmoidal_contrast")
 class sigmoidal_contrast (processing_cls):
 
         def __init__(self, negative=False, cutoff=0.5, gain=10):
-                pass
+                self._neg = negative
+                self._cutoff = cutoff
+                self._gain = gain
                 
         def process(self, img):
+                img = skimage.exposure.adjust_sigmoid(img, cutoff=self._cutoff, gain=self._gain, inv=self._neg)
+                print("Adjusted contrast.")
                 return [img]
